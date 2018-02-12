4cae49903602925ac9f2642352d214977129248c
sanzo1 <32183247+sanzo1@users.noreply.github.com>
2018-02-11 19:05:54 -0500

    Update BagLearner.py

--- BagLearner.py
+++ BagLearner.py
@@ -1,101 +1,70 @@
-"""Implement Bag Learner"""
-# TODO: Implement Boosting
-
 import numpy as np
-import LinRegLearner, DTLearner, RTLearner
+import random
 
 
-class BagLearner(object):
-
-    def __init__(self, learner, bags=20, boost=False, verbose=False, **kwargs):
-        """Initalize a Bag Learner
-
-        Parameters:
-        learner: A LinRegLearner, DTLearner, or RTLearner
-        bags: The number of learners to be trained using Bootstrap Aggregation
-        boost: If true, boosting will be implemented
-        verbose: If True, information about the learner will be printed out
-        kwargs: Keyword arguments to be passed on to the learner's constructor
-        
-        Returns: An instance of Bag Learner
-        """
-        self.verbose = verbose
-        learners = []
-        for i in range(bags):
-            learners.append(learner(**kwargs))
-        self.learners = learners
-        self.kwargs = kwargs
+class BagLearner(object) :
+    
+    def __init__(self, learner, kwargs = {"leaf_size":1}, bags=20, boost=False, verbose=False):
+        self.learner = learner
+        self.args = kwargs
         self.bags = bags
         self.boost = boost
-        if verbose:
-            self.get_learner_info()
-
+        self.verbose = verbose
         
-    def addEvidence(self, dataX, dataY):
-        """Add training data to learner
-
-        Parameters:
-        dataX: A numpy ndarray of X values to add
-        dataY: A numpy 1D array of Y values to add
-
-        Returns: Updated individual learners in BagLearner
-        """
-        # Sample the data with replacement
-        num_samples = dataX.shape[0]
-        for learner in self.learners:
-            idx = np.random.choice(num_samples, num_samples)
-            bagX = dataX[idx]
-            bagY = dataY[idx]
-            learner.addEvidence(bagX, bagY)
-        if self.verbose:
-            self.get_learner_info()
+        self.N = 0
+        self.learners=[]
+        self.leaf_size = 1
+        
+        if kwargs.has_key("leaf_size"):
+            self.leaf_size = kwargs['leaf_size']
         
         
-    def query(self, points):
-        """Estimates a set of test points given the model we built
+        if(self.bags == 0):
+            # If bag count is zero, set it to 1
+            self.bags =1
         
-        Parameters:
-        points: A numpy ndarray of test queries
 
-        Returns: 
-        preds: A numpy 1D array of the estimated values
-        """
-        preds = np.array([learner.query(points) for learner in self.learners])
-        return np.mean(preds, axis=0)
-
-
-    def get_learner_info(self):
-        """Print out data for this BagLearner"""
-        learner_name = str(type(self.learners[0]))[8:-2]
-        print ("This BagLearner is made up of {} {}:".
-            format(self.bags, learner_name))
-
-        print ("kwargs =", self.kwargs)
-        print ("boost =", self.boost)
-
-        # Print out information for each learner within BagLearner
-        for i in range(1, self.bags + 1):
-            print (learner_name, "#{}:".format(i)); 
-            self.learners[i-1].get_learner_info() 
-
-
-if __name__=="__main__":
-    print ("This is a Bag Learner\n")
-
-    # Some data to test the BagLearner
-    x0 = np.array([0.885, 0.725, 0.560, 0.735, 0.610, 0.260, 0.500, 0.320])
-    x1 = np.array([0.330, 0.390, 0.500, 0.570, 0.630, 0.630, 0.680, 0.780])
-    x2 = np.array([9.100, 10.900, 9.400, 9.800, 8.400, 11.800, 10.500, 10.000])
-    x = np.array([x0, x1, x2]).T
+        
+        for i in range(0,self.bags):
+            
+            self.learners.append(learner(**kwargs))
+            
     
-    y = np.array([4.000, 5.000, 6.000, 5.000, 3.000, 8.000, 7.000, 6.000])
-
-    # Create a BagLearner from given training x and y
-    bag_learner = BagLearner(DTLearner.DTLearner, verbose=True)
+    def author(self):
+        return 'pbaldwin6' # replace tb34 with your Georgia Tech username
     
-    print ("\nAdd data")
-    bag_learner.addEvidence(x, y)
+    def addEvidence(self,dataX,dataY):
+        self.N = dataX.shape[0]        
+        sampled_idx = np.random.choice(range(0,self.N) ,size = self.N, replace=True) 
+        trainX = dataX[sampled_idx, :]
+        trainY = dataY[sampled_idx]
+                
+        self.learners[0].addEvidence(trainX, trainY) 
 
-    # Query with dummy data
-    print ("Query with dummy data:\n", np.array([[1, 2, 3], [0.2, 12, 12]]))
-    print ("Query results:", bag_learner.query(np.array([[1, 2, 3], [0.2, 12, 12]]))) 
+        
+        for i in range(1,self.bags):
+            # Generate Bag 
+            if(self.boost) :
+                print "boosting not supported yet"
+            else:
+                sampled_idx = np.random.choice(range(0,self.N) ,size = self.N, replace=True) 
+                trainX = dataX[sampled_idx, : ]
+                trainY = dataY[sampled_idx]
+
+                self.learners[i].addEvidence(trainX, trainY)
+                
+
+    
+    
+    def query(self,points):
+        
+        bag_predY = np.zeros(points.shape[0])
+        for i in range(self.bags):
+            predY = self.learners[i].query(points)
+            bag_predY += predY
+        
+        bag_predY = bag_predY/self.bags
+        
+        return(bag_predY)
+        
+        
